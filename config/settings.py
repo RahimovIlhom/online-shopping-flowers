f143dca1f5a429caa794d653a97fc22ec7248911
RahimovIlhom <ilhomjonrahimov217@gmail.com>
2023-05-06 14:40:05 +0500

    ready to deploy

--- config/settings.py
+++ config/settings.py
@@ -133,9 +133,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
+# MEDIA
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
 
 # Default primary key field type
